6c76baa7d0cb1a382ba70d74118e30a5c4f48605
jigkoh3 <jigkoh3@hotmail.com>
2016-09-13 02:43:41 +0700

    make it better

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,11 @@
 var mongoose = require("mongoose");
-var uri = process.env.MONGOLAB_URI;
+var uri = "mongodb://heroku_bxndgfqt:heroku_bxndgfqt@ds029426.mlab.com:29426/heroku_bxndgfqt";
 
 
 module.exports = function(){
 	mongoose.set('debug',true);
 	var db = mongoose.connect(uri,function(err){
-		console.log(err);
+		console.log();
 	});
 
     require('../app/models/index.model');
