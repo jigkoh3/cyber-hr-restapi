6dfc09cf4f405d741dfdc19ac8e22cd8e69b9b3e
jigkoh3 <jigkoh3@hotmail.com>
2016-09-13 02:26:35 +0700

    make it better

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,9 @@ var uri = process.env.MONGOLAB_URI || "mongodb://localhost/hr";
 
 module.exports = function(){
 	mongoose.set('debug',true);
-	var db = mongoose.connect(uri);
+	var db = mongoose.connect(uri,function(err){
+		console.log(err);
+	});
 
     require('../app/models/index.model');
 
